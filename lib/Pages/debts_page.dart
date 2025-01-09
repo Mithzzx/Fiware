d3d900844ab79321260b3ff1044a783c4be5fed6
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2025-01-09 22:08:28 +0530

    bug

--- lib/Pages/debts_page.dart
+++ lib/Pages/debts_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../backend/database_helper.dart';
 import '../backend/debts.dart';
-import '../backend/accounts.dart';
 import 'debts_add_page.dart';
 
 class DebtsPage extends StatelessWidget {
